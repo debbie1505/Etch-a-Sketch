85b7e2b72efc0a70f4f111f83258b14a50d6e21b
Taiwo Omoya <taiwoomoya272@gmail.com>
2024-01-04 13:00:34 -0500

    Done with the project
    Implemented the javascript that changes the color of the boxes when
    the mouse enter the box. Made sure the size of the box remained the same
    even though the amount of box increase. Also implemented the prompt method
    that asks the user for the size and then the size changes based on the
    user's input.

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const gridContainer = document.querySelector(".grid-container");
+  function createGrid(sides) {
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.innerHTML = ""; // Clear the existing grid
 
-  for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row");
-    for (let j = 0; j < 16; j++) {
-      const gridItems = document.createElement("div");
-      gridItems.classList.add("grid");
-      gridRow.appendChild(gridItems);
+    for (let i = 0; i < sides; i++) {
+      const gridRow = document.createElement("div");
+      gridRow.classList.add("grid-row");
+
+      for (let j = 0; j < sides; j++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid");
+        gridRow.appendChild(gridItem);
+
+        //Adjust width and height when sides > 32
+
+        const newWidth = 500 / sides; // Calculate new width
+        const newHeight = 400 / (sides * 1.1); // Calculate new height
+        gridItem.style.width = `${newWidth}px`;
+        gridItem.style.height = `${newHeight}px`;
+
+        // Add event listener for hover effect
+        gridItem.addEventListener("mouseenter", function () {
+          gridItem.classList.add("grid-items-hover");
+        });
+      }
+
+      gridContainer.appendChild(gridRow);
     }
-
-    gridContainer.appendChild(gridRow);
   }
+
+  // Initial grid creation
+  createGrid(16);
+
+  const btn = document.querySelector(".myButton");
+  btn.addEventListener("click", function () {
+    let userInput = prompt(
+      "How many squares by side for the new grid (max: 100): "
+    );
+    userInput = parseInt(userInput, 10);
+
+    let sides = 0;
+
+    // Validate user input
+    if (!isNaN(userInput) && userInput > 0 && userInput <= 100) {
+      sides = userInput;
+      createGrid(sides);
+    } else {
+      alert("Invalid input. Please enter a number between 1 and 100.");
+    }
+  });
 });
